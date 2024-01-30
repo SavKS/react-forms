ea4f8c97c4a131dd325445874c508f9361d16b97
Andrii Savluk <a.savluk@ideil.com>
2024-01-30 12:42:38 +0200

    v1.7.0

--- src/components/ConsumeErrors.tsx
+++ src/components/ConsumeErrors.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useContext } from 'react';
+import { ReactNode, useContext, useMemo } from 'react';
 
 import { FormContext } from '../contexts/FormContext.js';
 import Form from '../Form.js';
@@ -9,6 +9,7 @@ type Props = {
     path: string | string[],
     form?: Form,
     config?: {
+        extract?: boolean,
         isRoot?: boolean
     },
     children: (errors: ValidationErrors | undefined) => ReactNode
@@ -23,7 +24,32 @@ export default function ConsumeErrors(props: Props) {
         throw new Error('Can\'t resolve form');
     }
 
-    const value = useFormErrors(form, props.path, props.config);
+    let value = useFormErrors(form, props.path, props.config);
+
+    value = useMemo(() => {
+        if (!props.config?.extract || !Object.keys(value).length) {
+            return value;
+        }
+
+        const extractPattern = (Array.isArray(props.path) ? props.path.join('.') : props.path).replace(
+            /\.\*$/,
+            ''
+        ).replaceAll(/\.\*/g, '.[\\w-]+');
+
+        if (!extractPattern) {
+            return value;
+        }
+
+        return Object.fromEntries(
+            Object.entries(value).map(([ key, value ]) => [
+                key.replace(
+                    new RegExp(`^${ extractPattern }`),
+                    ''
+                ).replace(/\.$/, '').replace(/^\./, ''),
+                value
+            ])
+        );
+    }, [ props.config?.extract, props.path, value ]);
 
     return (
         <>
