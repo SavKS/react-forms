0f148c47cac7bee06e2b5fb65127df976611c372
Andrii Savluk <sav1991@gmail.com>
2025-09-09 10:59:59 +0300

    v3.0.0-beta.6

--- src/hooks/useContextualForm.ts
+++ src/hooks/useContextualForm.ts
@@ -1,13 +1,26 @@
 import { useContext } from 'react';
 
 import { FormContext } from '../contexts/FormContext';
+import Form from '../Form';
 
-export default function useContextualForm() {
+function useContextualForm(opts: {
+    optional: true
+}): Form | undefined;
+
+function useContextualForm(opts?: {
+    optional?: false
+}): Form;
+
+function useContextualForm(opts?: {
+    optional?: boolean
+}) {
     const formContext = useContext(FormContext);
 
-    if (!formContext) {
+    if (!formContext && !opts?.optional) {
         throw new Error('Can\'t resolve form context.');
     }
 
     return formContext;
 }
+
+export default useContextualForm;
