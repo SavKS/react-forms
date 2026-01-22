059242a9dd91c7f08faafcb196bb2268b7e81eda
Andrii Savluk <sav1991@gmail.com>
2026-01-22 16:19:46 +0200

    v3.0.0-beta.10

--- src/utils/extractErrors.ts
+++ src/utils/extractErrors.ts
@@ -1,13 +1,25 @@
 import { ValidationErrors } from '../types';
 
-export default (errors: ValidationErrors, prefix: string) => {
+function extractErrors(errors: ValidationErrors, prefix: string, undefinedIfEmpty: true): ValidationErrors | undefined;
+
+function extractErrors(errors: ValidationErrors, prefix: string): ValidationErrors;
+
+function extractErrors(errors: ValidationErrors, prefix: string, undefinedIfEmpty?: boolean): ValidationErrors | undefined {
     const prefixRegExp = new RegExp(`^${ prefix }\\.`);
 
-    return Object.entries(errors).reduce((carry, [ key, messages ]) => {
+    const result = Object.entries(errors).reduce<ValidationErrors>((carry, [ key, messages ]) => {
         if (prefixRegExp.test(key)) {
             carry[ key.replace(prefixRegExp, '') ] = messages;
         }
 
         return carry;
-    }, {} as ValidationErrors);
-};
+    }, {});
+
+    if (undefinedIfEmpty && !Object.keys(result).length) {
+        return undefined;
+    }
+
+    return result;
+}
+
+export default extractErrors;
